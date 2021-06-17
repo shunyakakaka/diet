05d397b682258ef0902e9eedf02b3282c6bae3f5
shunyasaitama <85655151+shunyasaitama@users.noreply.github.com>
2021-06-17 17:35:15 +0900

    編集機能追加

--- app/controllers/diets_controller.rb
+++ app/controllers/diets_controller.rb
@@ -17,6 +17,17 @@ class DietsController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+
+    redirect_to diets_url
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :diets, only: [:index, :new, :show, :create]
+  resources :diets, only: [:index, :new, :show, :create, :edit, :update]
 end
